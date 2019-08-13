0683e41b9293211d4919b0e1f82a5e89d28f164b
201504481 <eljulio.arango97@gmail.com>
2019-08-13 12:43:31 -0600

    Servicio Cliente V1

---
+++ ServidorCliente/App_Code/EntidadCliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de EntidadCliente
+/// </summary>
+public class EntidadCliente
+{
+    private String _nombreUsuario;
+    private int _zonaActual;
+
+    public EntidadCliente(String nombreUsuario) {
+        _nombreUsuario = nombreUsuario;
+    }
+
+    public int GetZonaActual()
+    {
+        return _zonaActual;
+    }
+
+    public void SetZonaActual(String zonaActual)
+    {
+        _zonaActual = zonaActual;
+    }
+
+    public String GetNombreUsuario() {
+        return _nombreUsuario;
+    }
+
+    public void SetNombreUsuario(String nombreUsuario) {
+        _nombreUsuario = nombreUsuario;
+    }
+}
---
+++ ServidorCliente/App_Code/GestionCliente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de GestionCliente
+/// </summary>
+public class GestionCliente
+{
+    LinkedList<EntidadCliente> _listaClientes;
+    EntidadCliente _clienteActivo;
+
+    public GestionCliente() {
+        _listaClientes = new LinkedList<EntidadCliente>();
+    }
+
+    public int ObtenerZonaUsuario() 
+    {
+        if (_clienteActivo != null) 
+        {
+            return _clienteActivo.GetZonaActual();
+        }
+        else 
+        {
+            return 0;   
+        }   
+    }
+
+    public void RegistroCliente(String nombreUsuario)
+    {
+        foreach(EntidadCliente cliente in _listaClientes)
+        {
+            String nombreClienteActual = cliente.GetNombreUsuario();
+            if (nombreClienteActual.Equals(nombreUsuario)) 
+            {
+                _clienteActivo = cliente;
+                break;
+            }
+        }
+        _clienteActivo = null;
+    }
+}
