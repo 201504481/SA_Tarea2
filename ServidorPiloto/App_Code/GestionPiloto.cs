5f4b6be74d71fc7940d4b5028169b4ffb6798725
201504481 <eljulio.arango97@gmail.com>
2019-08-13 11:49:58 -0600

    Servicio Piloto V1

---
+++ ServidorPiloto/App_Code/GestionPiloto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de GestionPiloto
+/// </summary>
+public class GestionPiloto
+{
+    private LinkedList<EntidadPiloto> _listaPilotos;
+
+    public GestionPiloto() {
+        _listaPilotos = new LinkedList<EntidadPiloto>();
+    }
+
+    public void AgregarPiloto(EntidadPiloto piloto) {
+        _listaPilotos.AddLast(piloto);
+    }
+
+    public String ObtenerPilotos() {
+        String contenidoPilotos = "";
+        foreach (EntidadPiloto piloto in _listaPilotos) {
+            contenidoPilotos += "Codigo: " + piloto.GetCodigoPiloto() + " Estado: " + piloto.GetEstado() + " Zona Actual: " + piloto.GetZonaCubierta() + "\n";
+        }
+        return contenidoPilotos;
+    }
+}
